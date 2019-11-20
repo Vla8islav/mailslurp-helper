a4fd7825934ae575840df18975d5cd8937d45b93
Vladislav Pilipenko <vladislav.pilipenko@sravni.ru>
2019-11-20 21:28:46 +0300

    Fixed the index file

--- index.js
+++ index.js
@@ -5,24 +5,24 @@ const { MailSlurp: MailSlurpClient } = require('mailslurp-client');
 /**
  * Allows to use real emails in end 2 end tests via [MailSlurp service](https://mailslurp.com).
  * Sign up for account at MailSlurp to start.
- * 
+ *
  * A helper requires apiKey from MailSlurp to start
- * 
+ *
  * ```js
  * helpers: {
  *   MailSlurp: {
  *     apiKey: '<insert api key here>',
- *     require: '@codeceptjs/mailslurp-helper'    
+ *     require: '@codeceptjs/mailslurp-helper'
  *   },
  * }
  * ```
  * > Use .env file and environment variables to store sensitive data like API keys
- * 
+ *
  * ## Configuration
- * 
+ *
  * * `apiKey` (required) -  api key from MailSlurp
  * * `timeout` (default: 10000) - time to wait for emails in milliseconds.
- * 
+ *
  */
 class MailSlurp {
 
@@ -38,7 +38,7 @@ class MailSlurp {
     }
     this.mailslurp = new MailSlurpClient({ apiKey: this.config.apiKey, attribution: 'codeceptjs' });
   }
-  
+
   _before() {
     this.mailboxes = [];
     this.currentMailbox = null;
@@ -46,12 +46,17 @@ class MailSlurp {
   }
 
   async _after() {
-    if (!this.mailboxes.length) return;
+
+  	if (!this.mailboxes.length)
+    	return;
+
     await Promise.all(this.mailboxes.map(m => this.mailslurp.deleteInbox(m.id)));
+
     output.debug(`Removed ${this.mailboxes.length} mailboxes`);
+
     this.mailboxes = [];
     this.currentMailbox = null;
-    this.currentEmail = null;  
+    this.currentEmail = null;
   }
 
   // enterprise API function
@@ -65,7 +70,7 @@ class MailSlurp {
   /**
   * Creates a new mailbox. A mailbox will be deleted after a test.
   * Switches to last created mailbox.
-  * 
+  *
   * ```js
   * const mailbox = await I.haveNewMailbox();
   * ```
@@ -80,14 +85,14 @@ class MailSlurp {
 
   /**
   * Change a current mailbox to a provided one:
-  * 
+  *
   * ```js
   * const mailbox1 = await I.haveMailbox();
   * const mailbox2 = await I.haveMailbox();
   * // mailbox2 is now default mailbox
   * // switch back to mailbox1
   * I.openMailbox(mailbox)
-  * ``` 
+  * ```
   */
   openMailbox(mailbox) {
     this.currentMailbox = mailbox;
@@ -100,12 +105,12 @@ class MailSlurp {
 
   /**
   * Sends an email from current mailbox, created by `I.haveNewMailbox()`.
-  * 
+  *
   * ```js
   * I.sendEmail({
   *   to: ['user@site.com'],
   *   subject: 'Hello',
-  *   body: 'World'       
+  *   body: 'World'
   * });
   * ```
   */
@@ -117,19 +122,23 @@ class MailSlurp {
   /**
    * Waits for the first email in mailbox.
    * If mailbox is not empty - opens the last email.
-   * 
+   *
    * ```js
    * I.waitForLatestEmail()
    * // wait for 30 seconds for an email
    * I.waitForLatestEmail(30);
    * ```
-   * 
+   *
    * @param {num} [sec] Number of seconds to wait.
    * @returns {Promise<Email>} an email received.
    */
   async waitForLatestEmail(sec) {
-    if (sec) sec = 1000*sec;    
+
+    if (sec)
+    	sec = 1000*sec;
+
     const email = await this.mailslurp.waitForLatestEmail(this.currentMailbox.id, sec || this.config.timeout);
+
     printEmailDebug(email);
     this.currentEmail = email;
     return email;
@@ -137,91 +146,105 @@ class MailSlurp {
 
   /**
    * Wait for an exact email matched by query. You can match emails by `from`, `to`, `subject`, `cc`, `bcc` fields.
-   * My default, non-strcit matching enabled, so it searches for inclusion of a string. For a strict matching (equality) 
+   * My default, non-strcit matching enabled, so it searches for inclusion of a string. For a strict matching (equality)
    * prepend a value with `=` prefix.
-   * 
+   *
    * ```js
    *  // wait for email with 'password' in subject
    * const email = await I.waitForEmailMatching({
    *  subject: 'password',
    * });
-   * 
+   *
    * // wait 30 seconds for email with exact subject
    * const email = await I.waitForEmailMatching({
    *  subject: '=Forgot password',
    * }, 30);
-   * 
-   * // 
+   *
+   * //
    * const email = await I.waitForEmailMatching({
    *  from: '@mysite.com', // find anything from mysite
    *  subject: 'Restore password', // with Restore password in subject
    * });
    * ```
-   * @param {object} query to locate an email 
+   * @param {object} query to locate an email
    * @param {num} [sec] Number of seconds to wait.
    * @returns {Promise<Email>} an email received.
    */
   async waitForEmailMatching(query, sec) {
-    if (sec) sec = 1000*sec;
+    if (sec)
+    	sec = 1000*sec;
+
     const emailPreviews = await this.mailslurp.waitForMatchingEmails(
       matchEmailBy(query),
-      1, 
+      1,
       this.currentMailbox.id,
       sec || this.config.timeout
     );
+
     const email = await this.mailslurp.getEmail(emailPreviews[0].id);
+
     printEmailDebug(email);
+
     this.currentEmail = email;
     return email;
   }
 
   /**
   * Wait for exact number of emails in mailbox. Returns the last email in the list.
-  * 
+  *
   * ```js
   * // wait for 2 emails
   * I.waitForNthEmail(2);
   * // wait for 5 emails for 60 seconds
   * I.waitForNthEmail(5, 60);
   * // wait for 2 emails and return the last one
-  * const email = await I.waitForNthEmail(2); 
+  * const email = await I.waitForNthEmail(2);
   * ```
   */
   async waitForNthEmail(number, sec) {
-    if (sec) sec = 1000*sec;
+    if (sec)
+    	sec = 1000*sec;
+
     const email = await this.mailslurp.waitForNthEmail(this.currentMailbox.id, number, sec || this.config.timeout);
+
     this.currentEmail = email;
+
     printEmailDebug(email);
+
     this.currentEmail = email;
     return email;
   }
 
   /**
-   * Returns a bunch of emails matched by query. 
+   * Returns a bunch of emails matched by query.
    * Similar to `waitForEmailMatching` but returns an array of emails.
-   * 
+   *
    * ```js
    * // return 2 emails from 'user@user.com'
    * const emails = await I.grabEmailsMatching({ from: 'user@user.com'}, 2);
    * ```
-   * @param {object} query to locate an email 
+   * @param {object} query to locate an email
    * @param {num} [num] Number of emails to return.
    * @returns {Promise<[Email]>} emails matching criteria.
    */
   async grabEmailsMatching(query, num) {
+
     const emailPreviews = await this.mailslurp.waitForMatchingEmails(
       matchEmailBy(query),
-      num, 
+      num,
       this.currentMailbox.id,
       this.config.timeout
     );
+
+    console.log(emailPreviews);
+
     output.debug(`Received ${emailPreviews.length} emails`);
     return Promise.all(emailPreviews.map(e => this.mailslurp.getEmail(e.id)));
   }
 
   /**
    * Returns all emails from a mailbox.
-   * 
+   *
    * ```js
    * const emails = await I.grabAllEmailsFromMailbox();
    * ```
@@ -235,13 +258,13 @@ class MailSlurp {
 
   /**
   * Checks that current email subject contains a text.
-  * 
+  *
   * ```js
   * I.seeInEmailSubject('Restore password');
   * ```
-  * 
+  *
   * Requires an opened email. Use either `waitForEmail*` methods to open. Or open manually with `I.openEmail()` method.
-  */ 
+  */
   seeInEmailSubject(text) {
     this._hasCurrentEmail();
     const email = this.currentEmail;
@@ -250,11 +273,11 @@ class MailSlurp {
 
   /**
   * Checks that current email subject does not contain a text.
-  * 
+  *
   * ```js
   * I.seeInEmailSubject('Restore password');
   * ```
-  * 
+  *
   * Requires an opened email. Use either `waitForEmail*` methods to open. Or open manually with `I.openEmail()` method.
   */
   dontSeeInEmailSubject(text) {
@@ -265,13 +288,13 @@ class MailSlurp {
 
   /**
   * Checks that current email body contains a text.
-  * 
+  *
   * ```js
   * I.seeInEmailBody('Click link');
   * ```
-  * 
+  *
   * Requires an opened email. Use either `waitForEmail*` methods to open. Or open manually with `I.openEmail()` method.
-  */   
+  */
   seeInEmailBody(text) {
     this._hasCurrentEmail();
     const email = this.currentEmail;
@@ -280,13 +303,13 @@ class MailSlurp {
 
   /**
   * Checks that current email body does not contain a text.
-  * 
+  *
   * ```js
   * I.dontSeeInEmailBody('Click link');
   * ```
-  * 
+  *
   * Requires an opened email. Use either `waitForEmail*` methods to open. Or open manually with `I.openEmail()` method.
-  */    
+  */
   dontSeeInEmailBody(text) {
     this._hasCurrentEmail();
     const email = this.currentEmail;
@@ -295,11 +318,11 @@ class MailSlurp {
 
   /**
   * Checks that email is from a specified address.
-  * 
+  *
   * ```js
   * I.seeEmailIsFrom('user@user.com');
   * ```
-  * 
+  *
   * Requires an opened email. Use either `waitForEmail*` methods to open. Or open manually with `I.openEmail()` method.
   */
   seeEmailIsFrom(text) {
@@ -310,13 +333,13 @@ class MailSlurp {
 
   /**
   * Checks that current email subject equals to text.
-  * 
+  *
   * ```js
   * I.seeEmailSubjectEquals('Restore password');
   * ```
-  * 
+  *
   * Requires an opened email. Use either `waitForEmail*` methods to open. Or open manually with `I.openEmail()` method.
-  */   
+  */
   seeEmailSubjectEquals(text) {
     this._hasCurrentEmail();
     const email = this.currentEmail;
@@ -325,13 +348,13 @@ class MailSlurp {
 
   /**
   * Checks that current email subject doesn't equal to text.
-  * 
+  *
   * ```js
   * I.dontSeeEmailSubjectEquals('Restore password');
   * ```
-  * 
+  *
   * Requires an opened email. Use either `waitForEmail*` methods to open. Or open manually with `I.openEmail()` method.
-  */   
+  */
   dontSeeEmailSubjectEquals(text) {
     this._hasCurrentEmail();
     const email = this.currentEmail;
